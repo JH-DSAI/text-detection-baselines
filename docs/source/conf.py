768b5eef643b037146ef1159d582339237b1685d
Chandler May <chandler.camille.may@gmail.com>
2026-02-03 15:44:53 -0500

    Remove SSEC logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,8 +5,8 @@ from text_detection_baselines import __project__, __version__
 
 
 project = __project__
-copyright = '2023, SSEC-JHU'
-author = 'SSEC-JHU'
+copyright = '2026, JHU DSAI'
+author = 'JHU DSAI'
 
 release = __version__
 version = __version__
@@ -38,17 +38,17 @@ html_theme = 'sphinx_rtd_theme'
 #html_theme = 'sphinx_book_theme'
 html_static_path = ['../_static']
 html_css_files = ['../_static/custom.css']
-html_logo = '../_static/SSEC_logo_vert_white_lg_1184x661.png'
+#html_logo = '../_static/SSEC_logo_vert_white_lg_1184x661.png'
 html_title = f'{project} {release}'
 html_theme_options = {}
 if html_theme == 'sphinx_book_theme':
     html_theme_options.update({
         'logo': {
-            'image_light': '../_static/SSEC_logo_horiz_blue_1152x263.png',
-            'image_dark': '../_static/SSEC_logo_vert_white_lg_1184x661.png',
+            #'image_light': '../_static/SSEC_logo_horiz_blue_1152x263.png',
+            #'image_dark': '../_static/SSEC_logo_vert_white_lg_1184x661.png',
             'text': f'{html_title}',
         },
-        'repository_url': 'https://github.com/ssec-jhu/base-template',
+        'repository_url': 'https://github.com/JH-DSAI/text-detection-baselines',
         'use_repository_button': True,
     })
 
