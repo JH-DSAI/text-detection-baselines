bed11b9bdae16b7dfdc85f3b866d038b5da4e52b
Chandler May <chandler.camille.may@gmail.com>
2026-04-13 12:01:40 -0400

    Add type annotations to util.

--- text_detection_baselines/util.py
+++ text_detection_baselines/util.py
@@ -1,13 +1,23 @@
-import importlib
+"""General-purpose utilities."""
+
+import importlib.util
 import os
 from pathlib import Path
 
 from . import __project__  # Keep as relative for templating reasons.
 
 
-def find_package_location(package=__project__):
-    return Path(importlib.util.find_spec(package).submodule_search_locations[0])
+def find_package_location(package: str = __project__) -> Path:
+    """Return path to this package."""
+    pkg_spec = importlib.util.find_spec(package)
+    if pkg_spec is None:
+        raise RuntimeError(f"Could not find module spec for {package}")
+    search_locs = pkg_spec.submodule_search_locations
+    if search_locs is None:
+        raise RuntimeError(f"Could not find submodule search locations for {package}")
+    return Path(search_locs[0])
 
 
-def find_repo_location(package=__project__):
+def find_repo_location(package: str = __project__) -> Path:
+    """Return path to the text-detection-baselines repository."""
     return Path(find_package_location(package) / os.pardir)
